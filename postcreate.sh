9a416199fb7a0cb29e9da6b3b80c74ce9b88c9c7
Bjørn Einar Bjartnes <bjorn.einar.bjartnes@4subsea.com>
2022-07-15 23:55:10 +0200

    Things run now

--- postcreate.sh
+++ postcreate.sh
@@ -1,2 +1,3 @@
 cp -r /tools /workspaces/boosterblaster
 chmod +x ./tools/vice/bin/x64sc.exe
+chmod +x ./runProgramInEmulator.sh
---
+++ runProgramInEmulator.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#chmod +x ./tools/vice/bin/x64sc.exe main.prg
+./tools/vice/bin/x64sc.exe main.prg
