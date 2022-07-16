d28e5ec869d37868da2abf677701f3cc2e5c03f2
Bjørn Einar Bjartnes <bjorn.einar.bjartnes@4subsea.com>
2022-07-16 21:53:59 +0200

    Fix: Powershell script installed in too deep nested tools folder with vice-vice and kick kick

--- install_deps_windows.ps1
+++ install_deps_windows.ps1
@@ -1,8 +1,6 @@
-cd tools
 wget https://www.badgerpunch.com/booster/kickass.zip -OutFile kickass.zip
 wget https://www.badgerpunch.com/booster/vice.zip -OutFile vice.zip
-Expand-Archive kickass.zip
-Expand-Archive vice.zip
+Expand-Archive -DestinationPath tools kickass.zip
+Expand-Archive -DestinationPath tools vice.zip
 del kickass.zip
-del vice.zip
-cd ..
+kdel vice.zip
